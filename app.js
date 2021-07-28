f2022f6ec09a4576b65adbfd843388eb003d6efb
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2021-07-28 11:22:37 +0530

    change in javaScript file

--- app.js
+++ app.js
@@ -1,32 +1,51 @@
-var url = "https://api.funtranslations.com/translate/pirate.json";
-//var url = "https://api.funtranslations.com/translate/minion.json" ;
-var inputText = document.querySelector('.text-input');
-var outputText = document.querySelector('.text-output');
-var btn = document.querySelector('.btn');
 
-//console.log(url, inputText, outputText, btn);
+var inputText = document.querySelector(".text-input");
+var outputText = document.querySelector(".text-output");
+var btn = document.querySelector(".btn");
 
-function getTranslateUrl(text){
-    return url + "?" + "text=" + text;
-   
-}
 
-btn.addEventListener('click', doTranslate);
+btn.addEventListener('click', (e) => {
+  e.preventDefault()
+  let inputValue = inputText.value;
+  let url = `https://api.funtranslations.com/translate/pirate.json?text=${inputValue}`;
 
-function errorHandler(error){
-   alert("something wrong happen! please try agian later.", error);
-}
+  function errorHandler(err){
+    alert("something wrong happen! please try agian later.", err);
+  }
 
-function doTranslate(){
-   var inputTxt = inputText.value;
- 
+  fetch(url)
+  .then((res) =>{
+    return res.json()
+  })
+  .then((data) =>{
+    outputText.innerHTML = data.contents.translated
+  })
 
-   fetch(getTranslateUrl(inputTxt))
-   .then(response => response.json())
-   .then(json => {
-       var translatedTxt = json.contents.translated;
-       outputText.innerHTML = translatedTxt; 
-         })
+  .catch(errorHandler)
 
-   .catch(errorHandler)
-};
+})
+
+
+
+// function getTranslateUrl(text) {
+//   return url + "?" + "text=" + text;
+// }
+
+// btn.addEventListener("click", doTranslate);
+
+// function errorHandler(error) {
+//   alert("something wrong happen! please try agian later.", error);
+// }
+
+// function doTranslate() {
+//   var inputTxt = inputText.value;
+
+//   fetch(getTranslateUrl(inputTxt))
+//     .then((response) => response.json())
+//     .then((json) => {
+//       var translatedTxt = json.contents.translated;
+//       outputText.innerHTML = translatedTxt;
+//     })
+
+//     .catch(errorHandler);
+// }
